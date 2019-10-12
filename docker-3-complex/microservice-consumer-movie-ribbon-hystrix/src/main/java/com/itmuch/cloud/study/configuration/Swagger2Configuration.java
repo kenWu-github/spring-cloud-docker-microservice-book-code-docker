87f8110eaf42300ce56b0558561874226d09697b
吴俊杰 <wujunjie@52tt.com>
2019-10-12 18:46:17 +0800

    Swagger2Configuration.java

--- docker-3-complex/microservice-consumer-movie-ribbon-hystrix/src/main/java/com/itmuch/cloud/study/configuration/Swagger2Configuration.java
+++ docker-3-complex/microservice-consumer-movie-ribbon-hystrix/src/main/java/com/itmuch/cloud/study/configuration/Swagger2Configuration.java
@@ -1,7 +1,6 @@
 package com.itmuch.cloud.study.configuration;
 
 import com.github.xiaoymin.knife4j.spring.annotations.EnableSwaggerBootstrapUi;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -18,23 +17,20 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @EnableSwaggerBootstrapUi
 @Import(BeanValidatorPluginsConfiguration.class)
-@ConditionalOnProperty(name = "swagger2.enable", havingValue = "true")
 public class Swagger2Configuration {
     //swagger2的配置文件，这里可以配置swagger2的一些基本的内容，比如扫描的包等等
 
-
     @Bean
-    public Docket createWxkApi() {
+    public Docket createApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .groupName("default")
                 .select()
                 //为当前包路径
-                .apis(RequestHandlerSelectors.basePackage("com.itmuch.cloud.study"))
+                .apis(RequestHandlerSelectors.basePackage("com.itmuch.cloud.study.user"))
                 .paths(PathSelectors.any())
                 .build();
     }
-
 
     //构建 api文档的详细信息函数,注意这里的注解引用的是哪个
     private ApiInfo apiInfo() {

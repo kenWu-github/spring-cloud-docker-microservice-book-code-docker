f6ac941119993ecb289c10b8544fcb345d80ce46
吴俊杰 <wujunjie@52tt.com>
2019-10-12 19:35:32 +0800

    bugfix

--- docker-3-complex/microservice-consumer-movie-ribbon-hystrix/src/main/java/com/itmuch/cloud/study/ConsumerMovieApplication.java
+++ docker-3-complex/microservice-consumer-movie-ribbon-hystrix/src/main/java/com/itmuch/cloud/study/ConsumerMovieApplication.java
@@ -7,7 +7,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableCircuitBreaker
